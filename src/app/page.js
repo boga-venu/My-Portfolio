1b3354965e378d6ea92b1b709629aef152135074
Venu Gopal Boga <bogavenugopal96@gmail.com>
2025-02-21 19:44:47 +0530

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import Hero from '@/components/home/Hero'
+import About from '@/components/home/About'
 import Projects from '@/components/home/Projects'
 import Skills from '@/components/home/Skills'
 import Experience from '@/components/home/Experience'
@@ -26,6 +27,7 @@ export default function Home() {
       {mounted && (
         <SectionTransition>
           <div className="bg-white relative pt-12 z-20">
+            <About />
             <Projects />
             <Skills />
             <Experience />
