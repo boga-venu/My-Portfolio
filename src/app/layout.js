5c2b7122f294b816c771f6ae06cb7532194c6c34
Venu Gopal Boga <bogavenugopal96@gmail.com>
2025-02-21 19:44:19 +0530

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${outfit.variable}`}>
-      <body className="bg-white">
+      <body className="bg-white" suppressHydrationWarning>
         <Header />
         <main>{children}</main>
         <Footer />
